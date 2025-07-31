0f0fd74a5cdc5df0ec251f74265ff15406071f20
pandiyarajan-a <me.pandiyarajan@gmail.com>
2025-07-31 20:52:20 +0000

    update: workflow optimized

--- n8n_worflows/scripts/connect_msf_postgres.sh
+++ n8n_worflows/scripts/connect_msf_postgres.sh
@@ -4,4 +4,4 @@
 WORKSPACE=$(echo "$1" | tr "./" "_")
 
 # Run msfconsole to connect, create workspace, and import scan
-msfconsole -q -x "db_status; workspace -a $WORKSPACE; db_import /home/node/.n8n/result.xml; exit"
+msfconsole -q -x "db_status; workspace -a $WORKSPACE; db_import /home/node/.n8n/scans/$WORKSPACE/nmap_scan_result.xml; exit"
